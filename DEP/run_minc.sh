6ff8336f47c4715e42e477c231a7c37e55d899c4
SeunghyunLim <shl0216@dgist.ac.kr>
2020-07-31 15:56:25 +0900

    Update run_minc.sh

--- DEP/run_minc.sh
+++ DEP/run_minc.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1 python train.py --model DEPnet --dataset minc2500 --batch-size 64 --test-batch-size 64 --lr 0.01 --epochs 100
+CUDA_VISIBLE_DEVICES=0,1 python train_minc.py --model DEPnet --dataset minc2500 --batch-size 128 --test-batch-size 128 --lr 0.01 --epochs 100
